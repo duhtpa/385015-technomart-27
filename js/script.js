6aed274b5a097658e56dd046828b50f18d75f509
duhtpa <42221811+duhtpa@users.noreply.github.com>
2020-02-26 21:46:09 +0700

    доделал скрипты для всплывающей карты

--- js/script.js
+++ js/script.js
@@ -1,38 +1,33 @@
 var 	
 // Элементы вызова всплывающих окон
-feedbackBtn			= document.querySelector(".contacts_feedback-btn"), 	// кнопка "Заблудились? Напишите нам!"
-mapPreview			= document.querySelector(".contacts_map-link"), 			// превью карты
-goodsBuy				= document.querySelector(".goods_item_popup a"), 			// кнопка "Купить" в попапе карточки товара
+feedbackBtn			= document.querySelector(".contacts_feedback-btn"), // кнопка "Заблудились? Напишите нам!"
+mapPreview			= document.querySelector(".contacts_map-link"), 	// превью карты
+goodsBuy			= document.querySelector(".goods_item_popup a"), 	// кнопка "Купить" в попапе карточки товара
 // Форма обратной связи
-feedbackPopup		= document.querySelector(".feedback_popup"), 					// попап
-feedbackForm		= feedbackPopup.querySelector("form"), 								// форма с заполняемыми данными
-feedbackSend		= feedbackForm.querySelector(".feedback_send"), 					// кнопка "Отправить сообщение"
-userName				= feedbackForm.querySelector("#user_name"),								// поле "Имя пользователя"
-userEMail				= feedbackForm.querySelector("#user_email"),							// поле "Ваш e-mail"
-userMessage			= feedbackForm.querySelector("#user_message"),						// поле "Текст письма:"
+feedbackPopup		= document.querySelector(".feedback_popup"), 		// попап
+feedbackForm		= feedbackPopup.querySelector("form"), 				// форма с заполняемыми данными
+feedbackSend		= feedbackForm.querySelector(".feedback_send"), 	// кнопка "Отправить сообщение"
+userName			= feedbackForm.querySelector("#user_name"),			// поле "Имя пользователя"
+userEMail			= feedbackForm.querySelector("#user_email"),		// поле "Ваш e-mail"
+userMessage			= feedbackForm.querySelector("#user_message"),		// поле "Текст письма:"
 // Карта
-mapPopup				= document.querySelector(".map_popup"), 							// попап
-// Уведомление о добавлении товара в корзину - пусть будет!!!
-// purchasePopup		= document.querySelector(".purchase_popup"), 					// попап
-// purchaseCart		= document.querySelector(".purchase_cart"), 					// кнопка "Оформить заказ"
-// purchaseNext		= document.querySelector(".purchase_next"), 					// кнопка "Продолжить покупки"
+mapPopup			= document.querySelector(".map_popup"), 			// попап
+mapStatic			= document.querySelector(".map_popup-static");		// картинка-заглушка
 // Кнопка ЗАКРЫТЬ у всплывающих окон
-feedbackClose		= feedbackPopup.querySelector(".popup_close"),				// закрыть окно обратной связи
-mapClose				= mapPopup.querySelector(".popup_close"),							// закрыть развёрнутую карту
+feedbackClose		= feedbackPopup.querySelector(".popup_close"),		// закрыть окно обратной связи
+mapClose			= mapPopup.querySelector(".popup_close"),			// закрыть развёрнутую карту
 // LocalStorage
-isStorageSupport = true;
-storage		= "";
+isStorageSupport 	= true;												// дефолтное значение поддержки LocalStorage
+storage				= "";												// дефолтное значение сохраняемого параметра	
 
-try {
-	storage = localStorage.getItem("userName");
-} catch (err) {
-	isStorageSupport = false;
+try {																	// проверка (поддержки LocalStorage)...
+	storage = localStorage.getItem("userName");							// если данная инструкция выполнима - выполняем её...
+} catch (err) {															// иначе...
+	isStorageSupport = false;											// поддержки LocalStorage нет 
 }
 
-// var purchaseClose		= purchasePopup.querySelector(".popup_close");				// закрыть уведомление о покупке
-// а вот тут не знаю как выцепить клик по кнопке карточек
-
-//ФОРМА ОБРАТНОЙ СВЯЗИ
+//------------------------------------------------------
+//--------------ФОРМА ОБРАТНОЙ СВЯЗИ--------------------
 feedbackBtn.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	feedbackPopup.classList.add("popup-show");
@@ -46,10 +41,11 @@ feedbackBtn.addEventListener("click", function (evt) {
 feedbackForm.addEventListener("submit", function (evt) {
 	if (!userName.value || !userMessage.value) {
 		evt.preventDefault();
+		feedbackPopup.classList.remove("popup-error");
+		feedbackPopup.offsetWidth = feedbackPopup.offsetWidth;
 		feedbackPopup.classList.add("popup-error");
-		userName.style.borderColor = "#f15f5f";			// немного красоты
-		userMessage.style.borderColor = "#f15f5f";	// 
-		console.log("Не заполнены обязательные поля!");
+		userName.style.borderColor = "#f15f5f";
+		userMessage.style.borderColor = "#f15f5f";
 	} else {
 		if (isStorageSupport) {
 			localStorage.setItem("userName", userName.value);
@@ -61,23 +57,26 @@ feedbackForm.addEventListener("submit", function (evt) {
 feedbackClose.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	feedbackPopup.classList.remove("popup-show");
-	feedbackPopup.classList.add("popup-error");
+	feedbackPopup.classList.remove("popup-error");
 });
 
 // закрытие окна по Esc
 window.addEventListener("keydown", function (evt) {
 	if (evt.keyCode === 27) {
+		evt.preventDefault();
 		if (feedbackPopup.classList.contains("popup-show")) {
-			evt.preventDefault();
 			feedbackPopup.classList.remove("popup-show");
+			feedbackPopup.classList.remove("popup-error");
 		}
 	}
 });
 
-//КАРТА
+//-----------------------------------------------------------
+//-------------------------КАРТА-----------------------------
 mapPreview.addEventListener("click", function (evt) {
 	evt.preventDefault();
 	mapPopup.classList.add("popup-show");
+
 });
 
 // закрытие карты по кнопке
@@ -89,12 +88,9 @@ mapClose.addEventListener("click", function (evt) {
 // закрытие карты по Esc
 window.addEventListener("keydown", function (evt) {
 	if (evt.keyCode === 27) {
+		evt.preventDefault();
 		if (mapPopup.classList.contains("popup-show")) {
-			evt.preventDefault();
 			mapPopup.classList.remove("popup-show");
 		}
 	}
 });
-
-// localStorage.setItem("name", "value");
-
